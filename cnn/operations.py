dc764aea274fcf22ca55e117d9139a4b6fe4c7be
Chakkrit Termritthikun <kongpasom@gmail.com>
2019-01-04 14:00:29 +1030

    Update operations.py

--- cnn/operations.py
+++ cnn/operations.py
@@ -24,9 +24,9 @@ class ReLUConvBN(nn.Module):
   def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
     super(ReLUConvBN, self).__init__()
     self.op = nn.Sequential(
-      nn.ReLU(inplace=False),
       nn.Conv2d(C_in, C_out, kernel_size, stride=stride, padding=padding, bias=False),
-      nn.BatchNorm2d(C_out, affine=affine)
+      nn.BatchNorm2d(C_out, affine=affine),
+      nn.ReLU(inplace=False)
     )
 
   def forward(self, x):
@@ -37,10 +37,10 @@ class DilConv(nn.Module):
   def __init__(self, C_in, C_out, kernel_size, stride, padding, dilation, affine=True):
     super(DilConv, self).__init__()
     self.op = nn.Sequential(
-      nn.ReLU(inplace=False),
       nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=C_in, bias=False),
       nn.Conv2d(C_in, C_out, kernel_size=1, padding=0, bias=False),
       nn.BatchNorm2d(C_out, affine=affine),
+      nn.ReLU(inplace=False),
       )
 
   def forward(self, x):
@@ -52,7 +52,6 @@ class SepConv(nn.Module):
   def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
     super(SepConv, self).__init__()
     self.op = nn.Sequential(
-      nn.ReLU(inplace=False),
       nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding, groups=C_in, bias=False),
       nn.Conv2d(C_in, C_in, kernel_size=1, padding=0, bias=False),
       nn.BatchNorm2d(C_in, affine=affine),
@@ -60,6 +59,7 @@ class SepConv(nn.Module):
       nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=1, padding=padding, groups=C_in, bias=False),
       nn.Conv2d(C_in, C_out, kernel_size=1, padding=0, bias=False),
       nn.BatchNorm2d(C_out, affine=affine),
+      nn.ReLU(inplace=False),
       )
 
   def forward(self, x):
@@ -92,14 +92,14 @@ class FactorizedReduce(nn.Module):
   def __init__(self, C_in, C_out, affine=True):
     super(FactorizedReduce, self).__init__()
     assert C_out % 2 == 0
-    self.relu = nn.ReLU(inplace=False)
     self.conv_1 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
     self.conv_2 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False) 
-    self.bn = nn.BatchNorm2d(C_out, affine=affine)
+    self.bn = nn.BatchNorm2d(C_out, affine=affine),
+    self.relu = nn.ReLU(inplace=False)
 
   def forward(self, x):
     x = self.relu(x)
-    out = torch.cat([self.conv_1(x), self.conv_2(x[:,:,1:,1:])], dim=1)
+    out = torch.cat([self.conv_1(x), self.conv_2(x)], dim=1)
     out = self.bn(out)
+    out = self.relu(out)
     return out
-
