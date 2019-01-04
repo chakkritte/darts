73a3303875fdfc8be49bbfdd5f5ff6c8f1b45ada
Chakkrit Termritthikun <kongpasom@gmail.com>
2019-01-04 14:00:50 +1030

    Update operations.py

--- cnn/operations.py
+++ cnn/operations.py
@@ -12,10 +12,10 @@ OPS = {
   'dil_conv_3x3' : lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
   'dil_conv_5x5' : lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
   'conv_7x1_1x7' : lambda C, stride, affine: nn.Sequential(
-    nn.ReLU(inplace=False),
     nn.Conv2d(C, C, (1,7), stride=(1, stride), padding=(0, 3), bias=False),
     nn.Conv2d(C, C, (7,1), stride=(stride, 1), padding=(3, 0), bias=False),
-    nn.BatchNorm2d(C, affine=affine)
+    nn.BatchNorm2d(C, affine=affine),
+    nn.ReLU(inplace=False),
     ),
 }
 
